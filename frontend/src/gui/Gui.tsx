db59577ba081d1a6db3df062ab514e5dc5c3040c
LastSkywalkerr <maxdr1998@gmail.com>
2024-01-16 22:00:28 +0100

    gui improvement

--- frontend/src/gui/Gui.tsx
+++ frontend/src/gui/Gui.tsx
@@ -1,8 +1,9 @@
 import { KeyboardControlsEntry } from "@react-three/drei";
-import { FC, useEffect, useRef } from "react";
+import { FC, HTMLAttributes, ReactElement, ReactNode, useEffect, useRef } from "react";
 import cx from "classnames";
 import { ConnectWallet, useSigner } from "@thirdweb-dev/react";
 import { useGame } from "../shared/services/game.service";
+import classNames from "classnames";
 
 interface GuiProps {
   map: KeyboardControlsEntry<string>[];
@@ -10,6 +11,17 @@ interface GuiProps {
   isPlaying: boolean;
   setIsPlaying: (state: boolean) => void;
 }
+
+const GuiCard: FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...props }) => {
+  return (
+    <div
+      {...props}
+      className={cx("bg-gray-800 bg-opacity-75 rounded-lg p-4 border border-gray-600", className)}
+    >
+      {children}
+    </div>
+  );
+};
 
 export const Gui: FC<GuiProps> = ({ map, className, isPlaying, setIsPlaying }) => {
   const audio = useRef<HTMLAudioElement | null>(null);
@@ -26,53 +38,68 @@ export const Gui: FC<GuiProps> = ({ map, className, isPlaying, setIsPlaying }) =
     setIsPlaying(true);
   };
 
+  const pauseGame = () => {
+    setIsPlaying(false);
+  };
+
   const buyBullets = async () => {
     signer && (await buyGameBullets(signer, 100));
   };
 
   return (
-    <div className={cx("w-full h-full text-white flex", className)}>
-      <div className={"w-auto h-min bg-gray-800 bg-opacity-75 rounded-lg p-4 border border-gray-600"}>
-        <audio
-          ref={audio}
-          src="audios/ambient.mp3"
-          autoPlay
-          onEnded={(event) => event.currentTarget.play()}
-        />
-
-        <h1>Controls</h1>
-        {map.map(({ name, keys }) => (
-          <p key={name}>{`${name} - ${keys.map((key) => `${key} `)}`}</p>
-        ))}
-        {signer ? (
-          <>
-            <h1>Player</h1>
-            {isPlaying && <ConnectWallet />}
-            <p>{`Bullets - ${bullets}`}</p>
-            <p>{`Wrecked Ethers - ${wreckedEthers}`}</p>
-          </>
-        ) : null}
+    <div className={cx("", className)}>
+      <div className="fixed top-0 w-full items-start justify-between flex">
+        {signer && <ConnectWallet />}
+        {isPlaying && (
+          <GuiCard className="cursor-pointer py-1 px-2" onClick={pauseGame}>
+            X
+          </GuiCard>
+        )}
       </div>
 
       {!isPlaying && (
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-12">
-          {signer && (
-            <>
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                onClick={beginGame}
-              >
-                Play
-              </button>
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                onClick={buyBullets}
-              >
-                Buy Bullets
-              </button>
-            </>
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center">
+          {signer ? (
+            <div className="flex gap-12 items-center justify-center">
+              <GuiCard>
+                <audio
+                  ref={audio}
+                  src="audios/ambient.mp3"
+                  autoPlay
+                  onEnded={(event) => event.currentTarget.play()}
+                />
+
+                <h1>Controls</h1>
+                {map.map(({ name, keys }) => (
+                  <p key={name}>{`${name} - ${keys.map((key) => `${key} `)}`}</p>
+                ))}
+                {signer ? (
+                  <>
+                    <h1>Player</h1>
+
+                    <p>{`Bullets - ${bullets}`}</p>
+                    <p>{`Wrecked Ethers - ${wreckedEthers}`}</p>
+                  </>
+                ) : null}
+              </GuiCard>
+              <GuiCard className="flex flex-col gap-12">
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={beginGame}
+                >
+                  Play
+                </button>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={buyBullets}
+                >
+                  Buy Bullets
+                </button>
+              </GuiCard>
+            </div>
+          ) : (
+            <ConnectWallet />
           )}
-          <ConnectWallet />
         </div>
       )}
     </div>
